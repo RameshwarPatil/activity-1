8d5ef3479691a75e2c17f9f32fef17cbb0d4253e
RameshwarPatil <58586331+RameshwarPatil@users.noreply.github.com>
2019-12-06 16:27:26 +0530

    Create activity1.c

---
+++ activity1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+  
+int main()
+ {
+   
+ int n=10;
+    
+int i,n=10;
+    
+    
+unsigned long long factorial=1;
+    
+printf("enter ab integer:");
+    
+if(n<0)
+            
+printf("error! factorial of a negative number dorsn't exist.");
+
+   
+ else
+    
+{
+       
+ for(i=1;i<=n;i++)
+
+ for(i=1;i<n;++i)
+       
+ {
+            factorial*=i;
+
+        }
+
+}
+
+        return 0;
+
+}
